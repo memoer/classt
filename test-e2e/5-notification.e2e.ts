5f372372e33cb6412be308bb8688423cc9ae3faa
memoer <hanjn2842@naver.com>
2021-11-09 00:08:39 +0900

    test(notification): notification e2e test 완료

--- test-e2e/5-notification.e2e.ts
+++ test-e2e/5-notification.e2e.ts
@@ -1,7 +1,22 @@
 import { ApiTestBuilder } from './common/api-test.builder';
+import { getDataFromBody, testDescription } from './common/fn';
 
 export const notificationE2ETest = (apiBuilder: ApiTestBuilder): void =>
   describe('NotificationResolver [E2E]', () => {
-    it.todo('readNotification');
-    it.todo('readAllNotification');
+    it(
+      testDescription(
+        'mutation',
+        '학생은 자기가 받은 모든 알람을 한 번에 읽음 표시할 수 있어야 한다.',
+      ),
+      () =>
+        apiBuilder
+          .query('mutation', 'readAllNotification')
+          .includeToken('student')
+          .sendGql()
+          .expect(200)
+          .expect((res) => {
+            const isSuccess = getDataFromBody<boolean>(res, 'readAllNotification');
+            expect(isSuccess).toEqual(true);
+          }),
+    );
   });
